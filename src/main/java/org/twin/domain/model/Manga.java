3d3ccdc38a68fde2cd2fa506a22867884ac7b93d
Erick Oliveira <erick.loliveira@hotmail.com>
2024-06-06 15:29:02 -0300

    feat: manga added image url

--- src/main/java/org/twin/domain/model/Manga.java
+++ src/main/java/org/twin/domain/model/Manga.java
@@ -1,4 +1,24 @@
-package org.twin.domain.entity;
+package org.twin.domain.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Manga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "manga_gen")
+    @SequenceGenerator(name = "manga_gen", sequenceName = "manga_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+    private String title;
+    private String description;
+    private String imageUrl;
+    @Column(nullable = false)
+    private int chapter = 1;
 }
