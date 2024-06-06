aa6cda917faf3f4642ffd1e7253ee61d2d304e2a
Erick Oliveira <erick.loliveira@hotmail.com>
2024-06-06 18:27:35 -0300

    feat: unused import

--- src/main/java/org/twin/application/controller/MangaController.java
+++ src/main/java/org/twin/application/controller/MangaController.java
@@ -1,7 +1,6 @@
 package org.twin.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
